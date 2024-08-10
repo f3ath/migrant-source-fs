2f15a0b0f4cde921db84d4872d0f877dd43a71db
f3ath <karapetov@gmail.com>
2024-08-09 19:23:00 -0700

    0.0.3

--- lib/src/file_name_format.dart
+++ lib/src/file_name_format.dart
@@ -1,12 +1,12 @@
 /// Defines the format of the file name.
+/// All files containing the migrations must satisfy two rules:
+/// - the file name must begin with [versionFormat]
+/// - the file extension must match the [extension]
+///
+/// Examples of the version format:
+/// - `RegExp(r'\d{4}')` - a 4-digit number with leading zeros
+/// - `RegExp(r'\d{4}-\d{2}-\d{2}')` - a date
 class FileNameFormat {
-  /// All files containing the migrations must satisfy two rules:
-  /// - the file name must begin with [versionFormat]
-  /// - the file extension must match the [extension]
-  ///
-  /// Examples of the version format:
-  /// - `RegExp(r'\d{4}')` - a 4-digit number with leading zeros
-  /// - `RegExp(r'\d{4}-\d{2}-\d{2}')` - a date
   FileNameFormat(RegExp versionFormat, {String extension = '.sql'})
       : _extension = extension,
         _versionFormat = versionFormat;
@@ -17,14 +17,7 @@ class FileNameFormat {
 
   /// Matches the [fileName] against the format,
   /// returns the version if successful.
-  String? parseVersion(String fileName) {
-    if (!fileName.endsWith(_extension)) return null;
-    return _version(fileName);
-  }
-
-  /// Returns true if [version] has correct format.
-  bool isValidVersion(String version) => _version(version) == version;
-
-  String? _version(String value) =>
-      _versionFormat.matchAsPrefix(value)?.group(0);
+  String? parseVersion(String fileName) => fileName.endsWith(_extension)
+      ? _versionFormat.matchAsPrefix(fileName)?.group(0)
+      : null;
 }
--- test/functional_test.dart
+++ test/functional_test.dart
@@ -1,33 +1,29 @@
 import 'dart:io';
 
-import 'package:migrant/migrant.dart';
 import 'package:migrant_source_fs/migrant_source_fs.dart';
 import 'package:test/test.dart';
 
-void main() {
-  final source = LocalDirectory(
-      Directory('test/migrations'), FileNameFormat(RegExp(r'\d{4}')));
-  final m0 = Migration('0000', 'create table foo (id text not null);');
-  final m1 = Migration('0001', 'alter table foo add column name text;');
-  final m2 = Migration('0002', 'drop table foo;');
+void main() async {
+  final source = LocalFilesystem();
+  await source.load(Directory('test/migrations'), FileNameFormat(RegExp(r'\d{4}')));
 
-  test('Read', () async {
-    expect(await source.read().toList(), equals([m0, m1, m2]));
-    expect(await source.read(afterVersion: '0000').toList(), equals([m1, m2]));
-    expect(await source.read(afterVersion: '0001').toList(), equals([m2]));
-    expect(await source.read(afterVersion: '0002').toList(), isEmpty);
-    expect(await source.read(afterVersion: '0003').toList(), isEmpty);
+  group('Read', () {
+    test('First', () async {
+      final m0 = await source.getInitial();
+      expect(m0.version, equals('0000'));
+      expect(m0.statements, equals(['create table foo (id text not null);']));
+    });
+
+    test('Next', () async {
+      final m1 = await source.getNext('0000');
+      expect(m1?.version, equals('0001'));
+      expect(m1?.statements, equals(['alter table foo add column name text;']));
+      final m2 = await source.getNext('0001');
+      expect(m2?.version, equals('0002'));
+      expect(m2?.statements, equals(['drop table foo;']));
+      final m3 = await source.getNext('0002');
+      expect(m3, isNull);
+    });
   });
 
-  test('Invalid version format', () async {
-    expect(() => source.read(afterVersion: '').toList(), throwsFormatException);
-    expect(
-        () => source.read(afterVersion: 'abc').toList(), throwsFormatException);
-    expect(() => source.read(afterVersion: '00001').toList(),
-        throwsFormatException);
-    expect(() => source.read(afterVersion: '0000x').toList(),
-        throwsFormatException);
-    expect(() => source.read(afterVersion: 'x0000').toList(),
-        throwsFormatException);
-  });
 }
